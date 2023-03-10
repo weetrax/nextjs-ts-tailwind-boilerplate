c02ddf14e658859d037d7fed2c85fee5e4455382
smartines <stefano.martines@sequoiasoft.com>
2023-03-10 09:20:33 +0100

    sort import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
